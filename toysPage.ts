4c324ee42d069399bb1936023390dda7f693a237
Olena-web <73849375+Olena-web@users.noreply.github.com>
2021-12-20 16:14:29 +0200

    Add files via upload

--- toysPage.ts
+++ toysPage.ts
@@ -1,98 +1,171 @@
-import data from '../data.js';
+import data from '../data';
+import { MESSAGE } from '../constants/messages.constants';
+import { COUNT, YEAR, SHAPE, COLOR, SIZE, FAVORITE } from '../constants/toysPage.constants';
+import Control from '../common/control';
+import { findFavorite } from './filters';
+export const openWindow = document.querySelector<HTMLDivElement>('.pop-up-window');
 
-const isSelected = false;
-interface selectedItem {
-  num: string;
-  name: string;
-  count: string;
-  year: string;
-  shape: string;
-  color: string;
-  size: string;
-  favorite: string;
-  isChecked?: boolean;
-}
-interface selectedItems {
-  [index: number]: string;
-  selectedItem?: selectedItem[];
-}
 const toysContainer = document.querySelector<HTMLTemplateElement>('.toys-page__container');
+const resetBtnToys = document.querySelector<HTMLButtonElement>('.reset-toys');
+const resetBtn = document.querySelector<HTMLButtonElement>('.reset');
 
-function answerLovely(){
-  for (let i =0; i < data.length; i++){
-  let answer = data[i].favorite.toString();
-  if (answer == 'false') {
-    answer = 'да'
-  } else {
-    answer = 'нет'
+
+class Window extends Control {
+  constructor(parentNode: HTMLElement) {
+    super(parentNode);
+    const Window = new Control(this.node, 'div', 'pop-up-window', MESSAGE);
+    const closeBtn = new Control(this.node, 'div', 'close', ``);
+    Window.node.append(closeBtn.node);
+    return Window;
   }
- return answer;
-}}
+}
 
+function createWindow(): void {
+  if (toysContainer) {
+    const window = new Window(toysContainer);
+    toysContainer.prepend(window.node);
+    const openWindow = document.querySelector<HTMLDivElement>('.pop-up-window');
+    if (openWindow !== null) {
+      openWindow.classList.add('open');
+      const closeBtn = openWindow.querySelector<HTMLButtonElement>('.close');
+      if (closeBtn)
+        closeBtn.addEventListener('click', () => {
+          openWindow?.classList.remove('open');
+        });
+      if (resetBtnToys === null) throw Error;
+      resetBtnToys.addEventListener('click', () => {
+        openWindow?.classList.remove('open');
+      });
+    }
+  }
+}
 
-async function createToysContainer(): Promise<void> {
+export function createToysContainer(): void {
   if (toysContainer === null) {
     throw Error;
   } else {
+    toysContainer.innerHTML = '';
     data.forEach((item, i) => {
-      toysContainer.innerHTML += `<div class = "toys_item">
+      toysContainer.innerHTML += `<div class = "toys_item">\n
     <div class="ribbon">
-    <span></span></div>
-  <div class="title">${data[i].name}</div>
-  <img src="assets/toys/${data[i].num}.png" alt="toy">
-  <div class = "description">
-  <div class="count"> Количество: ${data[i].count}</div>
-  <div class="year"> Год покупки: ${data[i].year}</div>
-  <div class="shape"> Форма: ${data[i].shape}</div>
-  <div class="color">Цвет: ${data[i].color}</div>
-  <div class="size">Размер: ${data[i].size}</div>
-  <div class="favorite">Любимая: ${answerLovely()}</div>
-  </div>
+      <span></span>
+    </div>
+    <wrapper class = "card-wrapper">
+      <div class="title">${data[i].name}</div>
+      <button
+      /* .onclick = "function(){console.log('click button')}"*/ 
+      class="small minus-button"></button>
+    </wrapper>  
+    <img src="assets/toys/${data[i].num}.png" alt="${data[i].name}">    
+    <div class = "description">
+      <div class="count"> ${COUNT} ${data[i].count}</div>
+      <div class="year"> ${YEAR} ${data[i].year}</div>
+      <div class="shape"> ${SHAPE} ${data[i].shape}</div>
+      <div class="color">${COLOR} ${data[i].color}</div>
+      <div class="size">${SIZE} ${data[i].size}</div>
+      <div class="favorite">${FAVORITE} ${data[i].favorite}</div>
+    </div>
   </div>;
   `;
       const toysItem = document.querySelectorAll<HTMLDivElement>('.toys_item');
       const selectedSpan = document.querySelector<HTMLSpanElement>('.selected span');
       const selectedItems: string[] = [];
-      toysItem.forEach((item, i) => {
-        const ribbon = item.querySelector<HTMLDivElement>('.ribbon');
+      toysItem.forEach((item: HTMLDivElement, i: number) => {
         const selectedItem = data[i];
-        let count = parseInt(data[i].count);
-        function addToy(){
-          item.classList.toggle('selected-toy');
-          if (ribbon) ribbon.classList.toggle('ribbon-active');
+        let toysCount = parseInt(data[i].count);
+        const ribbon = item.querySelector<HTMLDivElement>('.ribbon');
+        const countDescr = item.querySelector<HTMLDivElement>('.count');
+        let countSelectedToys = 0;
+        const maxCountToys = 5; //for test, after change to 20;
+
+        function addToy(): void {
+          if (toysCount === 0) return;
+
+          if (selectedItems.length === maxCountToys) {
+            createWindow();
+            countSelectedToys = maxCountToys;
+            countSelectedToys = selectedItems.length;
+            if (selectedSpan !== null) selectedSpan.innerHTML = countSelectedToys.toString();
+            return;
+          }
+
+          toysCount--;
+
+          item.classList.add('selected-toy');
+          if (ribbon) ribbon.classList.add('ribbon-active');
           selectedItems.push(JSON.stringify(selectedItem));
-          if (selectedSpan !== null) selectedSpan.innerHTML = selectedItems.length.toString();
-          console.log(selectedItems.length); 
-          count = selectedItems.length;
-          console.log(count);
-         }
-        
-        if (!isSelected) {
-          item.addEventListener('click', () => {
-            addToy();
-            
-          }, 
-          );
-        } else {
-          item.removeEventListener('click', () => {
-            addToy();
-            
-          });
-          if (item.classList.contains('selected-toy')) {
-            item.addEventListener('click', () => {
-              item.classList.toggle('selected-toy')
-              if (ribbon) ribbon.classList.toggle('ribbon-active');
-              selectedItems.pop();
-              if (selectedSpan !== null) selectedSpan.innerHTML = selectedItems.length.toString();
-              console.log(selectedItems.length);
-              return selectedItems;
-              
-            });
+          countSelectedToys = selectedItems.length;
+          console.log(selectedItems);
+          if (selectedSpan !== null) selectedSpan.innerHTML = countSelectedToys.toString();
+          if (countDescr) countDescr.innerText = `${COUNT} ${toysCount.toString()}`;
+        }
+
+        function removeAllToy(): void {
+          toysCount = parseInt(data[i].count);
+          if (ribbon !== null) ribbon.classList.remove('ribbon-active');
+          if (selectedSpan !== null) selectedSpan.innerHTML = '0';
+          if (countDescr !== null) countDescr.innerText = `${COUNT} ${data[i].count}`;
+          selectedItems.length = 0;
+          if (openWindow) {
+            console.log(openWindow.classList);
+            openWindow.classList.remove('open');
           }
         }
-      });
-    });
-  }
-}
+
+        function removeToy(): void {
+          
+          selectedItems.forEach((item, i) => {
+            //if (toysCount === item.count );
+            toysCount++;
+            selectedItems.length--;
+            selectedItems.toString().replace(item[i].toString(), '');
+            console.log(selectedItems);
+            countSelectedToys = selectedItems.length;
+            if (selectedSpan !== null) selectedSpan.innerHTML = countSelectedToys.toString();
+            if (countDescr) countDescr.innerText = `${COUNT} ${toysCount.toString()}`;
+            if (ribbon !== null) ribbon.classList.toggle('ribbon-active');
+          });
+        }
+
+        item.addEventListener(
+          'click',
+          (e) => {
+            if(e.target == e.currentTarget) {
+            console.log('click');
+           // e.preventDefault();
+            addToy();
+          }},
+          //{ capture: true }
+        );
+        if (resetBtnToys === null) throw Error;
+        resetBtnToys.addEventListener('click', () => {
+          removeAllToy();
+        });
+        const minusBtn = document.querySelectorAll<HTMLButtonElement>('.minus-button');
+        if (minusBtn !== null) {
+          minusBtn.forEach((btn:HTMLButtonElement) => {
+            if (btn !== null){
+              btn.addEventListener('click', (e)=> {
+                if(e.target == e.currentTarget)
+               {
+                removeToy();
+                 e.stopPropagation();
+              }})
+            // btn.onclick = function(e)  {
+            //   if(e.target == e.currentTarget)
+            //   {console.log('click button');
+            //    removeToy();
+            //     e.stopPropagation();
+            //   }}};
+          }}
+          )}
+      
+      })
+    }) }}
 
 createToysContainer();
+
+if (resetBtn === null) throw Error;
+resetBtn.addEventListener('click', () => {
+  window.location.reload();
+});
