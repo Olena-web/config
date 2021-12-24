d011157b7b795de9d4fcab07a576a77056ceb860
Olena-web <73849375+Olena-web@users.noreply.github.com>
2021-12-24 17:05:01 +0200

    Add files via upload

--- treePage.ts
+++ treePage.ts
@@ -1,1 +1,11 @@
-import Control from '../common/control';
+//import Control from '../common/control';
+const treeItem = document.querySelectorAll<HTMLDivElement>('.menu-tree_item');
+console.log(treeItem);
+const mainTree = document.querySelector<HTMLDivElement>('.main-tree__containe');
+if (treeItem !== null && mainTree !== null) {
+  for (let i = 0; i < treeItem.length; i++) {
+    treeItem[i].addEventListener('click', () => {
+      mainTree.appendChild(img 'url(`../ assets / tree /${i}.png)`';)
+    });
+  }
+}
